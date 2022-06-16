29d43cbfb7dd9320a032883e4a772fbdd4d75f3a
MarcinMaster <m.luba505@gmail.com>
2022-06-16 07:03:29 +0200

    First commit of airqualityApp

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import SearchBar from "./components/Search /SearchBar";
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Search /SearchBar.js
@@ -0,0 +1,16 @@
+const SearchBar = () => {
+  return (
+    <div>
+      <form>
+        <div>
+          <input />
+        </div>
+        <div>
+          <button>Click</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SearchBar;

1164e91576d90bf0866e7e5223a0174beb8480d5
MarcinMaster <m.luba505@gmail.com>
2022-06-18 05:31:37 +0200

    Added a Nav and Foo

---
+++ src/components/UI/Footer.js
@@ -0,0 +1,9 @@
+import './Footer.css'
+
+const Footer = () => {
+ return (<div className="footer">
+    <p>Copyrigth 2022 COded by HappyHackers</p>
+ </div>)
+}
+
+export default Footer;
---
+++ src/components/UI/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.css'
+
+const Navbar = () => {
+ return ( <div className="navbar">
+    <p>Home</p>
+    <p>Contact</p>
+    <p>About</p>
+ </div>)
+}
+
+
+export default Navbar; 

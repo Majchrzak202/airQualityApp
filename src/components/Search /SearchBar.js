cb83f7623045c97ca9114407399c2883bb43101c
MarcinMaster <m.luba505@gmail.com>
2022-06-20 06:46:19 +0200

    Minor changes

--- src/components/Search /SearchBar.js
+++ src/components/Search /SearchBar.js
@@ -1,5 +1,5 @@
 import "./SearchBar.css";
-import { useState } from "react";
+import React,{ useState } from "react";
 
 const SearchBar = ({ searchHandler }) => {
   const [input, setInput] = useState("");
--- src/components/UI/Navbar.js
+++ src/components/UI/Navbar.js
@@ -1,11 +1,14 @@
+import React from 'react';
 import './Navbar.css'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
- return ( <div className="navbar">
-    <p>Home</p>
-    <p>Contact</p>
-    <p>About</p>
- </div>)
+ return ( <nav className="navbar">
+  <Link to='/'>Home</Link> 
+  <Link to='About'>About</Link>
+  <Link to='Contact'>Contact</Link>
+  <Link to='Info'>Info</Link>
+ </nav>)
 }
 
 
